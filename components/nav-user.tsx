a625862bb850877defbaf006d7d486545018a3ae
KongTrua <thung260803@gmail.com>
2025-05-16 14:51:43 +0700

    feat: implement user profile fetching and loading state in NavUser component

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -21,91 +21,128 @@ import {
 import { NavControls } from "./nav-controls";
 import { useRouter } from "next/navigation";
 import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { User } from "@/interfaces/user";
+import { processResponse } from "@/lib/response-process";
+import { Skeleton } from "./ui/skeleton";
 
 export function NavUser() {
   const { isMobile } = useSidebar();
   const router = useRouter();
   const { data: session } = useSession();
 
-  const user = {
-    name: session?.user?.name || "Guest",
-    email: session?.user?.email || "",
-    // You can add a default avatar or handle null case
-    avatar: session?.user?.image || "/placeholder.svg",
+  const [user, setUser] = useState<User>();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    if (session) {
+      const res = await fetch(`/api/user/profile`, {
+        method: "GET",
+      });
+      const response = await processResponse(res, {
+        success: false,
+        error: true,
+      });
+
+      if (response.success) {
+        setUser(response.data);
+      }
+    }
   };
+
+  useEffect(() => {
+    fetchData().then(() => setIsLoading(false));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session]);
 
   return (
     <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu modal={false}>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">
-                  {user.name.charAt(0).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="start"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="p-0 font-normal ">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+      {isLoading ? (
+        <Skeleton className="w-full h-12" />
+      ) : (
+        <SidebarMenuItem>
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarImage
+                    src={user?.profile?.avatar}
+                    alt={user?.username}
+                  />
                   <AvatarFallback className="rounded-lg">
-                    {user.name.charAt(0).toUpperCase()}
+                    {user?.username.charAt(0).toUpperCase()}
                   </AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate font-semibold">
+                    {user?.username}
+                  </span>
+                  <span className="truncate text-xs">{user?.email}</span>
                 </div>
-              </div>
-
-              <div className="flex md:hidden items-center gap-2">
-                <NavControls />
-              </div>
-            </DropdownMenuLabel>
-
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem onClick={() => router.push("/settings")}>
-                <Settings />
-                Settings
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                className="flex justify-between items-center"
-                onClick={() => router.push("/inbox")}
-              >
-                <div className="flex items-center gap-2">
-                  <Inbox />
-                  Inbox
+                <ChevronsUpDown className="ml-auto size-4" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+              side={isMobile ? "bottom" : "right"}
+              align="start"
+              sideOffset={4}
+            >
+              <DropdownMenuLabel className="p-0 font-normal ">
+                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                  <Avatar className="h-8 w-8 rounded-lg">
+                    <AvatarImage
+                      src={user?.profile?.avatar}
+                      alt={user?.username}
+                    />
+                    <AvatarFallback className="rounded-lg">
+                      {user?.username.charAt(0).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      {user?.username}
+                    </span>
+                    <span className="truncate text-xs">{user?.email}</span>
+                  </div>
                 </div>
-                <Dot className="text-sky-500" />
+
+                <div className="flex md:hidden items-center gap-2">
+                  <NavControls />
+                </div>
+              </DropdownMenuLabel>
+
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem onClick={() => router.push("/settings")}>
+                  <Settings />
+                  Settings
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="flex justify-between items-center"
+                  onClick={() => router.push("/inbox")}
+                >
+                  <div className="flex items-center gap-2">
+                    <Inbox />
+                    Inbox
+                  </div>
+                  <Dot className="text-sky-500" />
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => signOut()}>
+                <LogOut />
+                Log out
               </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => signOut()}>
-              <LogOut />
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      )}
     </SidebarMenu>
   );
 }
