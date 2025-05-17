6035b615b219e97bb74aea0489aba11b020b242b
KongTrua <thung260803@gmail.com>
2025-05-17 13:39:43 +0700

    feat: add development status message to landing page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -11,6 +11,9 @@ export default function Landing() {
     <div className="w-full h-full">
       <Nav />
       <div className="flex flex-col items-center justify-center h-full p-4 gap-8">
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+          This project is currently in development
+        </h4>
         <Image
           alt="image"
           src={"/placeholder.svg"}
