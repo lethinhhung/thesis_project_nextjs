fadfaab2f3dff7f5a2d506ec8c5395dd22a4b99d
KongTrua <thung260803@gmail.com>
2025-05-18 15:27:33 +0700

    feat: add GitHub repository links and enhance navigation in landing page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 import Nav from "@/components/landing-page-nav";
-import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
 
 export const metadata = {
   title: "Notebok",
@@ -10,17 +12,22 @@ export default function Landing() {
   return (
     <div className="w-full h-full">
       <Nav />
-      <div className="flex flex-col items-center justify-center h-full p-4 gap-8">
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-          This project is currently in development
-        </h4>
-        <Image
-          alt="image"
-          src={"/placeholder.svg"}
-          width={600}
-          height={300}
-          className="object-cover w-200 h-100 rounded-lg"
-        />
+      <div className="px-2 py-4 md:p-6 flex flex-col w-full min-h-[calc(100dvh-64px)]">
+        <div className="flex flex-col items-center justify-center space-y-4 m-auto">
+          <div className="p-4 md:p-6">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                  Notebok
+                </h1>
+                <p className="mx-auto max-w-175 text-muted-foreground md:text-xl">
+                  A personalized learning system intergrated with AI
+                </p>
+                <Badge variant={"destructive"}> Currently in development</Badge>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/landing-page-nav.tsx
+++ components/landing-page-nav.tsx
@@ -1,8 +1,17 @@
 "use client";
 
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Github } from "lucide-react";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import { DarkModeSwitcher } from "./dark-mode-switcher";
+import { LanguageSwitcher } from "./language-switcher";
+import { Separator } from "./ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Nav() {
   const router = useRouter();
@@ -11,9 +20,52 @@ export default function Nav() {
       <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
         Notebok
       </h4>
-      <Button onClick={() => router.push("/home")}>
-        Home <ArrowRight />
-      </Button>
+
+      <div className="flex items-center gap-2">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                target="_blank"
+                href={"https://github.com/lethinhhung/thesis_project_nextjs"}
+              >
+                <Button variant={"ghost"} size={"sm"}>
+                  <Github />
+                </Button>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Frontend Repo</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <a
+                target="_blank"
+                href={"https://github.com/lethinhhung/thesis_project_backend"}
+              >
+                <Button variant={"ghost"} size={"sm"}>
+                  <Github />
+                </Button>
+              </a>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Backend Repo</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <DarkModeSwitcher variant={"ghost"} size={"sm"} />
+        <LanguageSwitcher variant={"ghost"} size={"sm"} />
+        <Separator orientation="vertical" className="!h-8 mr-2" />
+        <Button onClick={() => router.push("/home")} size={"sm"}>
+          <p className="hidden sm:flex">Home</p>
+          <ArrowRight />
+        </Button>
+      </div>
     </div>
   );
 }
